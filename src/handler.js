e9ee703ab3b86c92ba50e435649ea393f9629eb7
Arwins-code <42213337+Arwins-code@users.noreply.github.com>
2021-07-23 01:03:15 +0700

    Update

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,136 @@
 const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
+  /**Client mengirim data catatan (title, tags, dan body) yang akan disimpan
+   * dalam bentuk JSON melalui body request. */
   const { title, tags, body } = request.payload;
 
   const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+  /**masukan nilai-nilai properti dari objek catatan ke dalam array notes
+   * menggunakan method push() */
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  //memberikan kondisi pada method isSuccess
+  /**Jika isSuccess bernilai true, maka beri respons berhasil dan sebaliknya */
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      error: false,
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    response.header('Access-Control-Allow-Origin', '*');
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: { notes },
+});
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+  /**Setelah mendapatkan nilai id, dapatkan objek note dengan id tersebut dari objek array notes.
+   * Manfaatkan method array filter() untuk mendapatkan objeknya. */
+  const note = notes.filter((n) => n.id === id)[0];
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: ' Catatan tidak ditemukan',
+  });
+  response.code(400);
+  return response;
+};
+
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Gagal berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan.',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,46 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`server berjalan pada ${server.info.uri}`);
 };
 
 init();
+
+/**penerapan CORS lebih mudah di Hapi cukup tambahkan pada spesifik route contoh:
+ * {
+  method: 'POST',
+  path: '/notes',
+  handler: addNoteHandler,
+  options: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+},
+ */
+
+/**Jika mau CORS di aktifkan di seluruh route cukup tambahkan CORS pada konfigurasi awal contoh:
+ * const server = Hapi.server({
+  port: 5000,
+  host: 'localhost',
+  routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+});
+ */
